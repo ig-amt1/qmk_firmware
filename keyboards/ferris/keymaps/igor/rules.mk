5f81bae270dae50921053e9a7fcbeb79e39b299f
ig-amt1 <109306583+ig-amt1@users.noreply.github.com>
2022-10-11 17:02:49 -0300

    Add files via upload

---
+++ keyboards/ferris/keymaps/igor/rules.mk
@@ -0,0 +1,11 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+
+COMBO_ENABLE    = yes
+
+LTO_ENABLE = yes
+QMK_SETTINGS = yes
+KEY_OVERRIDE_ENABLE = yes
+SEND_STRING_ENABLE = yes
+
+CAPS_WORD_ENABLE = yes
+
